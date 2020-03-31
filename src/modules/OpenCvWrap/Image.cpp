2deeda33f465fe633cc847fc8fdafdd52552e7df
akira <sunroad0418@gmail.com>
2020-03-31 22:50:05 +0800

    fix algorithm

--- src/modules/OpenCvWrap/Image.cpp
+++ src/modules/OpenCvWrap/Image.cpp
@@ -54,10 +54,51 @@ void Image::thinning() {
     auto start = chrono::steady_clock::now();
     int count = 0;
     bool hasRemovePixel = false;
-    bool removeDirection = true;
 
     while (true) {
-        image.forEach<uchar>([&hasRemovePixel, &removeDirection, this](const uchar &pixel, const int *position) {
+        image.forEach<uchar>([&hasRemovePixel, this](const uchar &pixel, const int *position) {
+            // ONLY REMOVE WHITE PIXEL
+            if (pixel == uchar(0)) {
+                return;
+            }
+            /**
+             *   P1  P2  P3
+             *   P4  P5* P6
+             *   P7  P8  P9
+             */
+            int centerX = position[0];
+            int centerY = position[1];
+
+            uchar p1 = getPixel(centerX - 1, centerY - 1);
+            uchar p2 = getPixel(centerX, centerY - 1);
+            uchar p3 = getPixel(centerX + 1, centerY - 1);
+            uchar p4 = getPixel(centerX - 1, centerY);
+            uchar p6 = getPixel(centerX + 1, centerY);
+            uchar p7 = getPixel(centerX - 1, centerY + 1);
+            uchar p8 = getPixel(centerX, centerY + 1);
+            uchar p9 = getPixel(centerX + 1, centerY + 1);
+
+            int nonZeroCount = p1 + p2 + p3 + p4 + p6 + p7 + p8 + p9;
+            int transformation =
+                    (!p1 && p2) +
+                    (!p2 && p3) +
+                    (!p3 && p6) +
+                    (!p6 && p9) +
+                    (!p9 && p8) +
+                    (!p8 && p7) +
+                    (!p7 && p4) +
+                    (!p4 && p1);
+
+            int right = p2 && p6 && p8;
+            int upper = p2 && p4 && p6;
+
+            if (nonZeroCount >= 2 && nonZeroCount <= 6 && (transformation == 1) && (right == 0) && (upper == 0)) {
+                setPixel(0, centerX, centerY);
+                hasRemovePixel = true;
+            }
+
+        });
+        image.forEach<uchar>([&hasRemovePixel, this](const uchar &pixel, const int *position) {
             // ONLY REMOVE WHITE PIXEL
             if (pixel == uchar(0)) {
                 return;
@@ -91,34 +132,22 @@ void Image::thinning() {
                     (!p7 && p4) +
                     (!p4 && p1);
 
-            int right = p2 && p6 && p8;
-            int upper = p2 && p4 && p6;
             int left = p2 && p4 && p8;
             int down = p4 && p6 && p8;
 
-
-            if (removeDirection) {
-                if (nonZeroCount >= 2 && nonZeroCount <= 6 && (transformation == 1) && (right == 0) && (upper == 0)) {
-                    setPixel(0, centerX, centerY);
-                    hasRemovePixel = true;
-                    removeDirection = !removeDirection;
-                }
-            } else {
-                if (nonZeroCount >= 2 && nonZeroCount <= 6 && (transformation == 1) && (left == 0) && (down == 0)) {
-                    setPixel(0, centerX, centerY);
-                    hasRemovePixel = true;
-                    removeDirection = !removeDirection;
-                }
+            if (nonZeroCount >= 2 && nonZeroCount <= 6 && (transformation == 1) && (left == 0) && (down == 0)) {
+                setPixel(0, centerX, centerY);
+                hasRemovePixel = true;
             }
+
         });
         count++;
 
-        if (! hasRemovePixel) {
+        if (!hasRemovePixel || count > 1000) {
             break;
         }
 
         hasRemovePixel = false;
-        removeDirection = true;
     }
 
     auto end = chrono::steady_clock::now();
